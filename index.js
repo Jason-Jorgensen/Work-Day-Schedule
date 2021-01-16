2628ba1d754728980eef92448c4fa9501261c9e1
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-01-15 21:58:24 -0700

    Adding HTML, JS and CSS sheets. HTML is built with rows and inputs to add daily tasks. CSS has styling selectors to change the color of rows dependent on that time of the day it is. JS changes the row's attributes according to the time of day and pushes to storage upon click of save button.

---
+++ index.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+    var showDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    var currentHour=moment().hour();
+    var totalHour = 18;
+    $("#currentDay").text(showDate);
+
+    for (let index = 8; index < totalHour; index++) {
+        var indexHour = index;
+        var hourBlock = "#"+indexHour+" .taskEntry";
+        if (currentHour > indexHour){
+            $(hourBlock).addClass("past");
+            // console.log(indexHour +"index Hour");
+            // console.log(hour + "hour")
+        }
+        else if (currentHour == indexHour){
+            $(hourBlock).addClass("present");
+        }
+        else{
+            $(hourBlock).addClass("future");
+        }
+    };
+    
+    function pushToStorage() {
+        
+        var selectedRow = $(this).parent().parent().attr("id");
+        var rowId = "#"+selectedRow+ " .taskEntry";
+        var workingSelector = $(rowId).children();
+        var inputText = $(rowId).children().val();
+        // console.log(inputText);
+        localStorage.setItem(rowId , JSON.stringify(inputText));
+        var fromLocalStorage = localStorage.getItem(rowId);
+        console.log(fromLocalStorage);
+        $(workingSelector).text(fromLocalStorage);
+        // var localStorageReturn = localStorage.getItem(rowId);
+        // $(workingSelector).HTML(localStorageReturn);
+        // console.log(localStorageReturn);
+        // console.log(workingSelector);
+    };
+
+    
+    
+    
+    $(document).on("click",".save", pushToStorage)
+    $(".clear").on("click",function(){
+        localStorage.clear();
+        location.reload();
+    })
+    
+
+});
+
